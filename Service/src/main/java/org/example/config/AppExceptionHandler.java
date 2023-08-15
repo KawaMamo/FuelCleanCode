52499545096e10c02c870be2a6d89012cd6e4207
KawaMamo <75807729+KawaMamo@users.noreply.github.com>
2023-08-15 15:51:30 +0300

    handle sql exception

--- Service/src/main/java/org/example/config/AppExceptionHandler.java
+++ Service/src/main/java/org/example/config/AppExceptionHandler.java
@@ -3,6 +3,7 @@ package org.example.config;
 import org.example.exceptions.DomainValidationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.mapping.PropertyReferenceException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.NoSuchElementException;
 
 @ControllerAdvice
@@ -34,5 +37,21 @@ public class AppExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<Object> handlePropertyReferenceException(PropertyReferenceException e, WebRequest request){
         logger.error(e.toString());
         return new ResponseEntity<>(e.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity<Object> handleSQLIntegrityConstraintViolationException(SQLIntegrityConstraintViolationException e, WebRequest request){
+        logger.error(e.toString());
+        return new ResponseEntity<>(e.getClass()+": "+e.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException e, WebRequest request){
+        logger.error(e.toString());
+        return new ResponseEntity<>(e.getClass()+": "+e.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(SQLException.class)
+    public ResponseEntity<Object> handleSQLException(SQLException e, WebRequest request){
+        logger.error(e.toString());
+        return new ResponseEntity<>(e.getClass()+": "+e.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST);
     }
 }
